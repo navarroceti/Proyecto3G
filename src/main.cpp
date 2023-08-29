25d6853fe02e984c485b7b2133544b4d5f13c670
navarroceti <129806280+navarroceti@users.noreply.github.com>
2023-08-29 01:50:54 +0000

    Se agrego libreria iostream

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
+    std::cout<<"Mensaje"<<::endl;
+
+    int a = 2;
     for (int nombre = 0; nombre < 10; nombre++)
     {
-
     }
-    
+
     return 0;
 }
